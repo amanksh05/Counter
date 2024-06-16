30c3edc2faeb2c2b063add4bdb267281dcc421f2
Aman Kumar <amanksh05@gmail.com>
2024-06-17 01:23:54 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [timer, setTimer] = useState(10)
+  const [disable, setDisable] = useState(false)
+
+  useEffect(() => {
+    if (timer > 0 && timer) {
+      const countdown = setInterval(() => {
+        setTimer(timer - 1);
+      }, 1000);
+      return () => clearInterval(countdown)
+    }
+    else {
+      setDisable(true)
+    }
+  }, [timer])
+
+  const hanadleClick = () => {
+    if (!disable)
+      setCount((count) => count + 1);
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>{timer<=0?`Time's up`:`Time remaining: ${timer}`}</h3>
+        <h4>{count}</h4>
+        <button onClick={hanadleClick}>Click me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
